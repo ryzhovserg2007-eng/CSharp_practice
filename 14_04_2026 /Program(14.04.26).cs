d924973a828bfd7e2331e9eb5b0300416f3f8fd6
ryzhovserg2007-eng <ryzhov.serg.2007@gmail.com>
2026-06-08 13:06:03 +0300

    Create Program(14.04.26).cs

---
+++ 14_04_2026 /Program(14.04.26).cs
@@ -0,0 +1,17 @@
+namespace ConsoleApp21
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Task11 t1 = new Task11("Hello, my dear-dear friend");
+            //Console.WriteLine(t1.Output);
+            Console.WriteLine(t1);
+            t1.Review();
+            //Console.WriteLine(t1.Output);
+            // string textOutput = t1.ToString();
+            //Console.WriteLine(t1.ToString);
+            Console.WriteLine(t1);
+        }
+    }
+}
